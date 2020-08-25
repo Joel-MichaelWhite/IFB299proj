874e412e9bf2331a515606f4e3776d1128233c21
akhanau <36717729+akhanau@users.noreply.github.com>
2020-08-25 16:34:10 +1000

    Creating a virtual environment with Django 2,1

---
+++ ifb299-venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\projects\uni\ifb299proj\ifb299-venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- musicschool/musicschool/settings.py
+++ musicschool/musicschool/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'USER': os.getenv("DATABASE_USER"),
         'PASSWORD': os.getenv("DATABASE_PASSWORD"),
         'HOST': os.getenv("DATABASE_HOST"),
-        'PORT': '',
+        'PORT': '3306',
     }
 }
 
